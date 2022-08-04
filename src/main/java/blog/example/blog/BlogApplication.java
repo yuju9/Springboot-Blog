1e18e9ef7b367e62c922074739763a3ba7a2a039
yuju9 <73828891+yuju9@users.noreply.github.com>
2022-08-04 10:43:04 +0900

    환경세팅 완료

---
+++ src/main/java/blog/example/blog/BlogApplication.java
@@ -0,0 +1,16 @@
+package blog.example.blog;
+
+import lombok.Getter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class BlogApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(BlogApplication.class, args);
+	}
+
+}
---
+++ src/main/java/blog/example/blog/Controller/HiController.java
@@ -0,0 +1,32 @@
+package blog.example.blog.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.sql.*;
+
+@Controller
+public class HiController {
+
+    public static void main(String[] args) {
+        try {
+            Connection conn = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306",
+                    "yuju",
+                    "1234"
+            );
+
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(
+                    "show databases"
+            );
+
+            while (rs.next()) {
+                System.out.println(rs.getString(1));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
