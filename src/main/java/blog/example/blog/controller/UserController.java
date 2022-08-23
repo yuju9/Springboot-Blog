788b43d415cf65f3d9ce7e3fb81f2dc002962ff6
yuju9 <73828891+yuju9@users.noreply.github.com>
2022-08-23 23:24:18 +0900

    13일차(null 오류)

--- src/main/java/blog/example/blog/controller/UserController.java
+++ src/main/java/blog/example/blog/controller/UserController.java
@@ -1,10 +1,23 @@
 package blog.example.blog.controller;
 
 import blog.example.blog.config.auth.PrincipalDetail;
+import blog.example.blog.model.KakaoProfile;
+import blog.example.blog.model.OAuthToken;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class UserController {
@@ -17,6 +30,71 @@ public class UserController {
     @GetMapping("/auth/loginForm")
     public String loginForm() {
         return "user/loginForm";
+    }
+
+    @GetMapping("/auth/kakao/callback")
+    public @ResponseBody String kakaoCallback(String code){
+
+        RestTemplate rt = new RestTemplate();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type", "authorization_code");
+        params.add("client_id", "96ff618336615bf0c4ab86b36d397df0");
+        params.add("redirect_uri", "http://localhost:8080/auth/kakao/callback");
+        params.add("code", code);
+
+        HttpEntity<MultiValueMap<String, String>> kakaoTokenReqeust =
+                new HttpEntity<>(params, headers);
+
+        ResponseEntity<String> response = rt.exchange(
+            "https://kauth.kakao.com/oauth/token",
+                HttpMethod.POST,
+                kakaoTokenReqeust,
+                String.class
+        );
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        OAuthToken oauthToken = null;
+        try {
+            oauthToken = objectMapper.readValue(response.getBody(), OAuthToken.class);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("카카오 엑세스 토큰" + oauthToken.getAccess_token());
+
+        RestTemplate rt2 = new RestTemplate();
+
+        HttpHeaders headers2 = new HttpHeaders();
+        headers2.add("Authorization", "Bearer " + oauthToken.getAccess_token());
+        headers2.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+
+        HttpEntity<MultiValueMap<String, String>> kakaoProfileReqeust =
+                new HttpEntity<>(headers2);
+
+        ResponseEntity<String> response2 = rt2.exchange(
+                "https://kapi.kakao.com/v2/user/me",
+                HttpMethod.POST,
+                kakaoProfileReqeust,
+                String.class
+        );
+
+        ObjectMapper objectMapper2 = new ObjectMapper();
+        KakaoProfile kakaoProfile = null;
+
+        try {
+            kakaoProfile = objectMapper2.readValue(response.getBody(), KakaoProfile.class);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("카카오 아이디:" + kakaoProfile.getId());
+        System.out.println("카카오 이메일:" + kakaoProfile.getKakao_account().getEmail());
+
+        return response2.getBody();
     }
 
     @GetMapping("/user/updateForm")
---
+++ src/main/java/blog/example/blog/model/KakaoProfile.java
@@ -0,0 +1,50 @@
+package blog.example.blog.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown=true)
+public class KakaoProfile {
+
+    public Integer id;
+    public String connected_at;
+    //public Properties properties;
+    public KakaoAccount kakao_account;
+
+//    @Data
+//    @JsonIgnoreProperties(ignoreUnknown=true)
+//    public class Properties {
+//
+//        public String nickname;
+//
+//    }
+
+    @Data
+    @JsonIgnoreProperties(ignoreUnknown=true)
+    public static class KakaoAccount {
+
+        //public Boolean profile_nickname_needs_agreement;
+        //public Profile profile;
+        public Boolean has_email;
+        public Boolean email_needs_agreement;
+        public Boolean is_email_valid;
+        public Boolean is_email_verified;
+        public String email;
+
+//        @Data
+//        @JsonIgnoreProperties(ignoreUnknown=true)
+//        public class Profile {
+//            public String nickname;
+//        }
+
+    }
+
+}
+
+
+
+
+
+
+
